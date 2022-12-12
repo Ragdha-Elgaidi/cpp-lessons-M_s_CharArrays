281233782087c7619f5d07f513745a189f7a9b21
Ragdha El-gaidi <76912120+Ragdha-Elgaidi@users.noreply.github.com>
2022-12-12 03:50:18 +0200

    Create Check and Convert Chars.cpp

---
+++ Check and Convert Chars.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	char ch1 = 'B';
+
+	if ('A' <= ch1 && ch1 <= 'Z') 
+	{
+		cout << ch1 << " is an upper case\n";
+		ch1 = ch1 - 'A' + 'a';
+		cout << ch1 << " now is a lower case\n";
+	}
+	else if ('a' <= ch1 && ch1 <= 'z')
+		cout << ch1 << " is already a lower case\n";
+	else if ('0' <= ch1 && ch1 <= '9')
+		cout << ch1 << " is a digit\n";
+	else
+		cout << ch1 << " is neither a digit nor a letter\n";
+
+	return 0;
+}
