d46716836620c1011e196272cfdedf11123ba416
Ragdha El-gaidi <76912120+Ragdha-Elgaidi@users.noreply.github.com>
2022-12-13 03:52:01 +0200

    Create read Array of strings .cpp

---
+++ read Array of strings .cpp
@@ -0,0 +1,18 @@
+/*
+● Using cin
+● Or use getline for spaces
+*/
+#include<iostream>
+using namespace std;
+
+int main()
+{
+
+	// Array of names - each name is sequence of letters!
+	string names[2];
+
+	for (int i = 0; i < 2; ++i)
+		cin >> names[i];
+
+	return 0;
+}
