bfa983600776ede40c2752f1471ae1765b12debf
Ragdha El-gaidi <76912120+Ragdha-Elgaidi@users.noreply.github.com>
2022-12-13 03:49:00 +0200

    Create Array of strings.cpp

---
+++ Array of strings.cpp
@@ -0,0 +1,18 @@
+/*
+● Using cin
+● Or use getline for space
+*/
+#include<iostream>
+using namespace std;
+
+int main()
+{
+
+	// Array of names - each name is sequence of letters!
+	string names[5] = { "Mostafa Saad", "Never Ever", "Hello world" };
+
+	for (int i = 0; i < 5; ++i)
+		cout << names[i] << "\n";
+
+	return 0;
+}
